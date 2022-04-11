952aef78ecd9500c5307092b358f1a0ede55542c
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2022-04-11 11:06:52 +0200

    menu 1v

---
+++ src/com/company/Menu.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Menu {
+
+
+    public static void  handleMenu() {
+
+        displayMenu();
+        Scanner in = new Scanner(System.in);
+        String choose = "";
+
+        while(true) {
+            choose = in.nextLine();
+
+            switch (choose) {
+                case "1":
+                    displayMenu();
+                    break;
+                case "2":
+                    displayMenu();
+                    break;
+                case "3":
+                    displayMenu();
+                    break;
+                case "4":
+                    displayMenu();
+                    break;
+                case "5":
+                    displayMenu();
+                    break;
+                case "6":
+                    System.out.println("Quiting...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("There is no such command!");
+                    displayMenu();
+                    break;
+            }
+        }
+
+
+    }
+
+    private static void  displayMenu() {
+
+        System.out.println("1. Select Test Data ");
+        System.out.println("2. Select Naive Bayes Classification");
+        System.out.println("3. Select kNN Classification");
+        System.out.println("4. Select Metric");
+        System.out.println("5. Start Classification");
+        System.out.println("6. Quit");
+
+    }
+
+
+
+}
